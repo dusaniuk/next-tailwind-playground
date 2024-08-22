7fb666ac23c781a0f85d989f80817c964de548b3
yaroslav <20459871+dusaniuk@users.noreply.github.com>
2024-08-22 16:51:00 +0300

    chore: small fix for classnames

--- src/libs/evento/events/EventCard/EventCardSkeleton.tsx
+++ src/libs/evento/events/EventCard/EventCardSkeleton.tsx
@@ -5,7 +5,7 @@ export function EventCardSkeleton() {
     <div className="relative flex flex-col flex-1 rounded-xl overflow-hidden bg-white/[3%] basis-80 h-[23.75rem] max-w-[31.25rem] state-active">
       <Skeleton className="h-[60%] rounded-b-none" />
       <div className="flex flex-col flex-1 justify-center items-center">
-        <Skeleton className="w-[80%] h-6 " />
+        <Skeleton className="w-[80%] h-6" />
         <Skeleton className="w-[60%] h-4 mt-4" />
         <Skeleton className="w-[40%] h-4 mt-4" />
       </div>
